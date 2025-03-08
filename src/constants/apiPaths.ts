22f331be3e0096496c35fbd5d678d11cacff6bfd
Evgeney Savostyanov <24965432+Evgeney-S@users.noreply.github.com>
2025-03-09 01:18:54 +0400

    API PATHS updated for import dervice

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://zciru1s5m0.execute-api.eu-north-1.amazonaws.com/prod",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://t1tk8v11c1.execute-api.eu-north-1.amazonaws.com/prod",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };  
