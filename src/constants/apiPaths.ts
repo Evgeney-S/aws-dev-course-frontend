67d8e8c63fa4c0ad98351580daa8f53de2d2f40a
Evgeney Savostyanov <24965432+Evgeney-S@users.noreply.github.com>
2025-04-20 16:33:40 +0400

    Task 10 (Backend For Frontend)

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@ const API_PATHS = {
   product: "https://zciru1s5m0.execute-api.eu-north-1.amazonaws.com/prod",
   order: "https://naf73xh4tsogjvgw253stfpzey0zvack.lambda-url.eu-north-1.on.aws/api",
   import: "https://t1tk8v11c1.execute-api.eu-north-1.amazonaws.com/prod",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  bff: "http://evgeney-s-bff-api-production.eu-north-1.elasticbeanstalk.com",
   cart: "https://naf73xh4tsogjvgw253stfpzey0zvack.lambda-url.eu-north-1.on.aws/api",
 };  
 
